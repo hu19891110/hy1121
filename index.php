19690519b47ba7998a4028f5d20347a05c6f3d81
houyi1121 <houyi1121@163.com>
2015-09-08 08:40:26 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- index.php
+++ index.php
@@ -345,30 +345,4 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
-<script>
-/*-------------------------------------------------*/
-/*  在页面上单击时，出现积分的特效
-/*-------------------------------------------------*/
-jQuery(document).ready(function($) {
-$("html,body").click(function(e){
-	var n=Math.round(Math.random()*100);//随机数
-	var $i=$("<b/>").text("+"+n);//添加到页面的元素
-	var x=e.pageX,y=e.pageY;//鼠标点击的位置
-	$i.css({
-		"z-index":99999,
-		"top":y-20,
-		"left":x,
-		"position":"absolute",
-		"color":"#E94F06"
-	});
-	$("body").append($i);
-	$i.animate(
-		{"top":y-180,"opacity":0},
-		1500,
-		function(){$i.remove();}
-	);
-	e.stopPropagation();
-});
-});
-</script>
 
